1e48fab9db29f5b25aba41050dd1d148d0f85016
Kitling <kitling@kitling.ktarian.com>
2016-09-10 16:14:30 -0700

    Add MMU stuff. Will this work? (please work)

--- ntrcard.c
+++ ntrcard.c
@@ -8,10 +8,28 @@
 #include "pins.h"
 
 extern void dummy(unsigned);
+extern void start_mmu ( unsigned int, unsigned int );
+extern void stop_mmu ( void );
+extern void invalidate_tlbs ( void );
+
+#define MMUTABLEBASE 0x00004000
+
+unsigned int mmu_section ( unsigned int vadd, unsigned int padd, unsigned int flags )
+{
+    unsigned int ra;
+    unsigned int rb;
+    unsigned int rc;
+
+    ra=vadd>>20;
+    rb=MMUTABLEBASE|(ra<<2);
+    rc=(padd&0xFFF00000)|0xC00|flags|2;
+    *(uint32_t*)rb = rc;
+    return(0);
+}
 
 struct ntr_state {
     uint8_t currentRawCmd[8];
-    enum ntrcard_command command;
+    uint8_t command;
 };
 
 static struct ntr_state state;
@@ -49,6 +67,18 @@ void ntr_write_buffer(const uint8_t *data, uint32_t size) {
 }
 
 int pimain(void) {
+    for(unsigned ra=0;;ra+=0x00100000) {
+        mmu_section(ra,ra,0x0000);
+        if(ra==0xFFF00000) break;
+    }
+    mmu_section(0x00000000,0x00000000,0x0000|8|4);
+    mmu_section(0x00100000,0x00100000,0x0000|8|4);
+    mmu_section(0x00200000,0x00200000,0x0000|8|4);
+    mmu_section(0x20000000,0x20000000,0x0000); //NOT CACHED!
+    mmu_section(0x20100000,0x20000000,0x0000); //NOT CACHED!
+    mmu_section(0x20200000,0x20200000,0x0000); //NOT CACHED!
+    start_mmu(MMUTABLEBASE,0x00000001|0x1000|0x0004);
+
     initpins();
     debugCount = 0;
 
