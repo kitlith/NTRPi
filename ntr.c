cbabfbc71a7e243902c83efa18c4f878ad256fca
Kitling <kitling@kitling.ktarian.com>
2016-09-10 21:34:16 -0700

    More structuring.

--- ntr.c
+++ ntr.c
@@ -2,11 +2,14 @@
 
 uint32_t bytes;
 extern uint8_t CS1;
+extern uint8_t CLK;
 
 int pimain(void) {
     // init
     while (1) {
         while (1) { // Read command
+            while (CLK); // Set by interrupt
+            CLK = 1;
             // Do stuff.
             if (bytes >= 8) { // Finished reading command
                 // Get ready to Output
@@ -14,9 +17,13 @@ int pimain(void) {
             }
         }
         while (1) { // Output data
+            while (CLK) {
+                if (CS1) break;
+            } // Set by interrupt
             if (CS1) { // Set by interrupt
+                CS1 = 0;
                 // Get ready to input
-                break;
+                break; // Do this checking in a better place?
             }
             // Do stuff
         }
