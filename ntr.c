aa86ea1d25159a96591cf41687821c4470281f34
Kitling <kitling@kitling.ktarian.com>
2016-09-12 16:21:26 -0700

    Added licence under GPLv3.

--- ntr.c
+++ ntr.c
@@ -1,3 +1,19 @@
+/* Copyright 2016 Kitlith                                                *
+ * Licensed under GPLv3 or any later version.                            *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include <stdint.h>
 
 #include "registers.h"
@@ -5,7 +21,7 @@
 
 const uint8_t chipid[4] = {'H', 'E', 'L', 'P'};
 
-extern void (*c_irq_handler)(void); // TODO: Headers, pls.
+extern void (*c_irq_handler)(void);
 volatile uint8_t cs1_triggered;
 
 uint32_t bytes;
